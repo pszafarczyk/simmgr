f04a26e57d2fc20c117bb44eeb86f85b7de435b3
Piotr Szafarczyk <dev@netexpert.pl>
2025-07-31 16:49:08 +0200

    NC/feat: add retries to applying to target

--- net-configurator/src/net_configurator/developer.py
+++ net-configurator/src/net_configurator/developer.py
@@ -17,6 +17,8 @@ from net_configurator.discrepancy_finder import FilterDiscrepancyFinder
 from net_configurator.discrepancy_finder import OwnerDiscrepancyFinder
 from net_configurator.discrepancy_finder import RuleDiscrepancyFinder
 from net_configurator.optimizer import Optimizer
+from net_configurator.rule import Owner
+from net_configurator.rule import PacketFilter
 from net_configurator.rule import Rule
 from net_configurator.rules_source import ReaderFactoryInterface
 from net_configurator.rules_source import RulesSource
@@ -32,7 +34,11 @@ TARGET_RETRY_TIMEOUT = 800
 TARGET_RETRY_DELAY = 200
 
 
-class SourceReaderError(Exception):
+class SourceError(FatalError):
+    """Exception raised when problems with reading rules from source."""
+
+
+class TargetError(FatalError):
     """Exception raised when problems with reading rules from source."""
 
 
@@ -69,49 +75,31 @@ class Developer:
         """(Re)creates rules target."""
         self.__target = RulesTarget(self.__target_factory.create())
 
-    def process(self) -> None:  # noqa: C901
-        """Changes target to match source."""
-        desired_rules = self.__read_source_rules()
-        with self.__target:
-            existing_rules = self.__target.read_all_rules()
-            existing_filters = self.__target.read_all_filters()
-            existing_owners = self.__target.read_all_owners()
-            self.__logger.info('%d rules read from target', len(existing_rules))
+    def run(self) -> None:
+        """Alters target to match source.
 
-            optimizer = Optimizer(desired_rules)
-            optimizer.optimize()
-            desired_rules = optimizer.get_rules()
-            desired_filters = optimizer.get_filters()
-            desired_owners = optimizer.get_owners()
+        Raises:
+            SourceError: When reading from source not possible.
+            TargetError: When applying to target not possible.
+        """
+        desired_rules = self.__read_source_rules_with_retries()
 
-            rule_discrepancy_finder = RuleDiscrepancyFinder(desired_rules, existing_rules)
-            filter_discrepancy_finder = FilterDiscrepancyFinder(desired_filters, existing_filters)
-            owner_discrepancy_finder = OwnerDiscrepancyFinder(desired_owners, existing_owners)
+        optimizer = Optimizer(desired_rules)
+        optimizer.optimize()
+        desired_rules = optimizer.get_rules()
+        desired_filters = optimizer.get_filters()
+        desired_owners = optimizer.get_owners()
 
-            for rule_identifier in rule_discrepancy_finder.get_elements_to_delete():
-                self.__target.delete_rule(rule_identifier)
-            for packet_filter_identifier in filter_discrepancy_finder.get_elements_to_delete():
-                self.__target.delete_filter(packet_filter_identifier)
-            for owner_identifier in owner_discrepancy_finder.get_elements_to_delete():
-                self.__target.delete_owner(owner_identifier)
-
-            for owner in owner_discrepancy_finder.get_elements_to_add():
-                self.__target.add_owner(owner)
-            for packet_filter in filter_discrepancy_finder.get_elements_to_add():
-                self.__target.add_filter(packet_filter)
-            for rule in rule_discrepancy_finder.get_elements_to_add():
-                self.__target.add_rule(rule)
-
-            self.__target.apply_changes()
+        self.__apply_source_to_target_with_retries(desired_rules, desired_filters, desired_owners)
         self.__logger.info('Target successfully updated')
 
-    def __read_source_rules(self) -> set[Rule]:  # type: ignore[return]
-        """Returns rules from source.
+    def __read_source_rules_with_retries(self) -> set[Rule]:  # type: ignore[return]
+        """Returns rules from source retrying if necessary.
 
         In case of recoverable errors tries the operation source_retry_count times.
 
         Raises:
-            SourceReaderError: When reading from source not possible.
+            SourceError: When reading from source not possible.
 
         Returns:
             set[Rule]: Set of rules read from source.
@@ -131,10 +119,81 @@ class Developer:
                     before=recreate_before_retry,
                 ):
                     with attempt, self.__source:
-                        desired_rules = self.__source.read_all_rules()
-                        self.__logger.info('%d rules read from source', len(desired_rules))
-                        return desired_rules
+                        return self.__read_source_rules()
             except RetryError:
                 pass
         except (RecoverableError, FatalError) as e:
-            raise SourceReaderError from e
+            raise SourceError from e
+
+    def __read_source_rules(self) -> set[Rule]:
+        """Returns rules from source.
+
+        Raises:
+            Exception: Exceptions raised by read_all_rules of source handler.
+
+        Returns:
+            set[Rule]: Set of rules read from source.
+        """
+        desired_rules = self.__source.read_all_rules()
+        self.__logger.info('%d rules read from source', len(desired_rules))
+        return desired_rules
+
+    def __apply_source_to_target_with_retries(self, desired_rules: set[Rule], desired_filters: set[PacketFilter], desired_owners: set[Owner]) -> None:
+        """Applies necessary changes to target retrying if necessary.
+
+        In case of recoverable errors tries the operation target_retry_count times.
+
+        Raises:
+            TargetError: When applying to target not possible.
+        """
+
+        def recreate_before_retry(retry_state: RetryCallState) -> None:  # noqa: ARG001
+            self.__recreate_target()
+
+        try:
+            try:
+                for attempt in Retrying(
+                    stop=(stop_after_attempt(self.target_retry_count) | stop_after_delay(self.target_retry_timeout)),
+                    wait=wait_fixed(self.target_retry_delay),
+                    retry=retry_if_exception_type(RecoverableError),
+                    reraise=True,
+                    before_sleep=before_sleep_log(self.__logger, logging.WARNING),
+                    before=recreate_before_retry,
+                ):
+                    with attempt, self.__target:
+                        self.__apply_source_to_target(desired_rules, desired_filters, desired_owners)
+            except RetryError:
+                pass
+        except (RecoverableError, FatalError) as e:
+            raise TargetError from e
+
+    def __apply_source_to_target(self, desired_rules: set[Rule], desired_filters: set[PacketFilter], desired_owners: set[Owner]) -> None:  #  noqa: C901
+        """Applies necessary changes to target.
+
+        Raises:
+            Exception: Exceptions raised by methods of target handler.
+        """
+        existing_rules = self.__target.read_all_rules()
+        existing_filters = self.__target.read_all_filters()
+        existing_owners = self.__target.read_all_owners()
+        self.__logger.info('%d rules read from target', len(existing_rules))
+
+        rule_discrepancy_finder = RuleDiscrepancyFinder(desired_rules, existing_rules)
+        filter_discrepancy_finder = FilterDiscrepancyFinder(desired_filters, existing_filters)
+        owner_discrepancy_finder = OwnerDiscrepancyFinder(desired_owners, existing_owners)
+
+        for rule_identifier in rule_discrepancy_finder.get_elements_to_delete():
+            self.__target.delete_rule(rule_identifier)
+        for packet_filter_identifier in filter_discrepancy_finder.get_elements_to_delete():
+            self.__target.delete_filter(packet_filter_identifier)
+        for owner_identifier in owner_discrepancy_finder.get_elements_to_delete():
+            self.__target.delete_owner(owner_identifier)
+
+        for owner in owner_discrepancy_finder.get_elements_to_add():
+            self.__target.add_owner(owner)
+        for packet_filter in filter_discrepancy_finder.get_elements_to_add():
+            self.__target.add_filter(packet_filter)
+        for rule in rule_discrepancy_finder.get_elements_to_add():
+            self.__target.add_rule(rule)
+
+        self.__target.apply_changes()
